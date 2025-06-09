6d0f1cfd81d4902531c0479c645c812100c2e883
Daniel-choi-11 <chtropikal@gmail.com>
2025-06-09 17:28:35 +0900

    Update 01.py

--- Practice/01.py
+++ Practice/01.py
@@ -1,1 +1,25 @@
+grades = {
+    "Huang": {"Test":71},
+    "John": {"Test":65},
+    "Jung": {"Test":100},
+    "Darryl": {"Test":89}
+}
+if grades["Huang"]["Test"] <= 70:
+    print("시험 탈락")
+else:
+    print("시험 합격")
 
+if grades["John"]["Test"] <= 70:
+    print("시험 탈락")
+else:
+    print("시험 합격")
+
+if grades["Jung"]["Test"] <= 70:
+    print("시험 탈락")
+else:
+    print("시험 합격")
+
+if grades["Darryl"]["Test"] <= 70:
+    print("시험 탈락")
+else:
+    print("시험 합격")
