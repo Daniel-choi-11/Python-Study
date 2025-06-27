7e69c0a52d6531934e5debcf41a10d2542d1b8ec
Sunwook Lee <developsun04@gmail.com>
2025-06-27 17:15:25 +0900

    Update def.py

--- def.py
+++ def.py
@@ -8,7 +8,7 @@
 def 함수_이름(매개변수):
     수행할_문장1...
 
-#매개변수란 함수의 입력값또는 input이라 생각하면 편하다.
+#매개변수란 함수의 입력 값 또는 input이라 생각하면 편하다.
 
 def add(a, b): 
     return a + b
@@ -18,23 +18,3 @@ c=add(a, b)
 print(c)
 
 print(add(10, 4))
-
-=======================
-
-def add_without_return(a, b):
-    result = a + b # a 변수와 b 변수에 있는 값을 result 라는 변수에 저장해줘
-    print(result) # result 변수에 있는 값을 출력해줘
-
-total = add_without_return(3, 5) # return 값이 없기 때문에 total 이라는 변수에 아무 값도 저장이 안됨
-print(total) # None 출력
-
-================================
-
-# 두 수의 합을 반환하는 함수 예제
-def add(a, b):
-    result = a + b # a 변수와 b 변수에 있는 값을 result 라는 변수에 저장해줘
-    print(result) # result 변수에 있는 값을 출력해줘
-    return result # result 변수에 있는 값을 반환해줘
-
-total = add(3, 5) # return 값이 있던 덕분에 total 이라는 변수에 결과 값이 저장 됨
-print(total) # 8 출력
