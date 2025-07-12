a99f70050f24d219c62b7c4112a2d86d76bcd67e
Daniel-choi-11 <chtropikal@gmail.com>
2025-07-12 14:14:37 +0900

    Update 06. ~Class01.py

--- Practice/06. ~Class01.py
+++ Practice/06. ~Class01.py
@@ -1,1 +1,16 @@
+class calculator:
+    def addition(self,a,b):
+        return a+b
+    def subtraction(self,a,b):
+        return a-b
+    def multiplication(self,a,b):
+        return a*b
+    def division(self,a,b):
+        return a/b
+    
 
+a=3
+b=4
+self=addition(a,b)
+print(self)
+print(addition(3,4))
