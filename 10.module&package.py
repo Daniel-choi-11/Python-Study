0fbbbde8bf0a520c9a6a050dfa21ac4472b5cacb
Daniel-choi-11 <chtropikal@gmail.com>
2025-07-25 17:57:10 +0900

    Update and rename 10.module.py to 10.module&package.py

---
+++ 10.module&package.py
@@ -0,0 +1,22 @@
+# module
+# module이란 함수나 변수또는 class를 모아둔 python 파일이다
+
+# import module_이름
+# module을 불러올때 사용되는 방법 
+
+# from module_이름 import module_함수
+# module에서 특정 함수만 불러올때 사용하는 방법
+
+
+# package
+# 다른 사람이 만든 코드
+# pip install 패키지 이름
+
+# pip list -> 설치된 패키지 목록 확인하기
+# pip install --upgrade "패키지 이름" -> 패키지 업그레이드
+# pip uninstall "패키지 이름" -> 패키지 지우기
+
+# 패키지란?
+# 패키지란 여러가지의 모듈을 하나의 디렉토리에 모아놓은것이다. 이를 통해 코드 재사용성을 높이고 관리하기쉽게 도와준다
+
+
