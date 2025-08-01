a3bcaa8026ce0c863f6481221cfd9a97b8309eaf
Sunwook Lee <developsun04@gmail.com>
2025-08-01 17:57:42 +0900

    Update 08. ~Class03.py

--- Practice/08. ~Class03.py
+++ Practice/08. ~Class03.py
@@ -1,3 +1,16 @@
+'''
+시차 계산기
+- 원하는 나라들간의 시차를 구하기 위해 나라 입력 가능
+- 나라입력을 하면 시간대 출력 (ex. 나라이름= GMT+9)
+
+코드 순서
+- 각 나라들 시간대 저장
+- input으로 나라를 넣으면 시간대 출력 기능 만들기
+
+- input으로 원하는 2를 넣은다음, 나라들 간의 시차 계산기능 만들기
+- 
+'''
+
 import pytz
 from datetime import datetime
 for tz in pytz.all_timezones:
@@ -22,7 +35,14 @@ def timezone(userinput):
     if userinput in Country:
         return f"{userinput}의 시간대는: {now_tz}"
 
+'''
+문제점 및 개선점
+1. userinput과 Country 딕셔너리
+userinput에는 나라 이름(예: "한국")을 넣어야 하고, 이걸 Country 딕셔너리에서 타임존 문자열로 변환해야 함.
 
+2. pytz.timezone에 직접 나라 이름을 넣으면 안 됨.그리고  "Asia/Seoul" 같은 타임존 문자열이 들어가야 함.
+지금 코드는 userinput(= "한국" 등)를 직접 넣어서 에러가 남.
+'''
 
 def calculator(userinput1, userinput2):
     if userinput1 not in Country:
@@ -32,18 +52,6 @@ def calculator(userinput1, userinput2):
     if userinput1, userinput2 in 
 
 
-'''
-시차 계산기
-- 원하는 나라들간의 시차를 구하기 위해 나라 입력 가능
-- 나라입력을 하면 시간대 출력 (ex. 나라이름= GMT+9)
-
-코드 순서
-- 각 나라들 시간대 저장
-- input으로 나라를 넣으면 시간대 출력 기능 만들기
-
-- input으로 원하는 2를 넣은다음, 나라들 간의 시차 계산기능 만들기
-- 
-'''
 
 # 좋은 버전
 def timezone(country):
