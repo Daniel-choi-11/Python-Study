8c59b66bbd5862d341df66cc3c2a5cee929e5fdd
Daniel-choi-11 <chtropikal@gmail.com>
2025-08-09 11:18:36 +0900

    Update 09. Inputoutput.py

--- Practice/09. Inputoutput.py
+++ Practice/09. Inputoutput.py
@@ -1,1 +1,19 @@
+with open("StudentGrade.txt", "w") as file:
+    file.write("최정의 성적: 100점\n")
+    file.write("지호의 성적: 80점\n")
+    file.write("현호의 성적: 85점\n")
+    file.write("시후의 성적: 90점")
 
+with open("StudentGrade.txt", "r") as file:
+    a=file.read()
+
+print(a)
+
+
+# 좋은 예시
+'''
+with open("grades.txt", "r") as file:
+    for line in file:
+        name, grade = line.strip().split(": ")
+        print(f"{name}의 성적: {grade}")
+'''
